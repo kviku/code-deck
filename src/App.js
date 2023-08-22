0498499b354cbff4e85737458f079d4f764ec265
kviku <KVIKU98@GMAIL.COM>
2023-08-22 23:48:28 +0530

    add tailwind

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// 31 min 
+function App() {
+  return (
+    <div>
+     lets build code-deck
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#1e90ff",
+        secondary:"#ff6347",
+        black: "#lelele",
+        white:"#ffffff",
+        
+      }
+    },
+
+  },
+  plugins: [],
+}
+
